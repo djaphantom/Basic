9afc862e20dc46b553a20bb00ee9d58efe410193
djaphantom <djacksonfelix71@gmail.com>
2022-05-06 21:36:45 -0300

    Inicio do controle do usuario
    foram implementadas as classes Usuario permição e GrupoUsuario.
    
    foi dado inicio a implementação da classe usuario.
    
    Missão para proxima aula terminar a implementação da classe Usuario BLL e fazer o CRUD no banco de dados

---
+++ Basic/DLL/UsuarioBll.cs
@@ -0,0 +1,32 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class UsuarioBll
+
+    {
+        public void Inserir()
+        {
+
+        }
+        public void excluir()
+        {
+
+        }
+        public void Alterar()
+        {
+
+        }
+        public List<Usuario> Buscar()
+        {
+            return new List<Usuario>();
+
+
+        }
+    }
+}
---
+++ Basic/Model/Usuario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Usuario
+    {
+        private int id;
+        private string nomeUsuario;
+        private string senha;
+        private bool ativo;
+
+        public string NomeUsuario
+        {
+            get { return nomeUsuario; }
+            set { nomeUsuario = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public bool Ativo
+        {
+            get { return ativo; }
+            set { ativo = value; }
+        }
+
+        public string Senha
+        {
+            get { return senha; }
+            set { senha = value; }
+        }
+
+
+    }
+}
