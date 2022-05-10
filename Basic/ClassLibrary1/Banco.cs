a172d94e91cbce82182131afe274ab58441ce131
djaphantom <djacksonfelix71@gmail.com>
2022-05-09 22:02:59 -0300

    crição_do_pacote_DAL
    criação do pacote DAL e conecção com o banco de dados.
    bug na Class Banco

---
+++ Basic/ClassLibrary1/Banco.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    readonly SqlTransaction tt = null;
+    readonly SqlConnection cn = null;
+
+    public class Banco
+    {
+        public Banco()
+
+        {
+
+            t = cn.BeginTransaction(IsolationLevel.Serializable);
+            cn = new SqlConnection("");
+        }
+        public bool ExecutarComando(List<ComandoSQL> cmd)
+        {
+
+            foreach (var item in cmd)
+            {
+                item.Comando.Transaction = t;
+                foreach (var p in item.Parametros)
+                {
+                    item.Comando.Parameters.AddWithValue(p.Parametro, p.Valor);
+                }
+                item.Comando.ExecuteNonQuery();
+
+            }
+
+            t.Commit();
+            return true;
+        }
+    } 
+        
+}
+
